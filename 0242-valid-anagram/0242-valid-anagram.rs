7252840403d6521ec133522e4ddaef0ab9563851
Raphael Nembhard <100111587+DeluxeRaph@users.noreply.github.com>
2023-08-23 23:28:16 -0400

    Time: 4 ms (46.11%), Space: 2.4 MB (36.5%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if (s.len() != t.len()){
+            return false;
+        }
+        let mut map: HashMap<char, i64> = HashMap::new();
+
+        for (a, b) in s.chars().zip(t.chars()){
+            *map.entry(a).or_default() += 1;
+            *map.entry(b).or_default() -= 1;
+        }
+        map.into_values().all(|cnt| cnt == 0)
+    }
+}
