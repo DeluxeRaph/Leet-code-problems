511259f909c268cd926635bec56ad80aa6e0d984
Raphael Nembhard <100111587+DeluxeRaph@users.noreply.github.com>
2023-08-26 21:33:01 -0400

    Time: 5 ms (98.85%), Space: 8.2 MB (5.07%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,37 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct { // Creating hash table
+    int key;
+    int val;
+
+    UT_hash_handle hh;
+} hash_table;
+
+hash_table *hash = NULL, *elem, *tmp;
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* res = calloc((*returnSize = 2), sizeof(int));
+
+    for(int i = 0; i < numsSize; ++i) {
+        int k = target - nums[i];
+
+        HASH_FIND_INT(hash, &k, elem); // Look for the item in the hashtable
+        if(elem) {
+            res[0] = elem->val;
+            res[1] = i;
+            break;
+        }
+        else {
+            elem = malloc(sizeof(hash_table));
+            elem->key = nums[i]; // array element as key of hash table
+            elem->val = i; // index of an element as value of hash table
+
+            HASH_ADD_INT(hash, key, elem); 
+        }
+    }
+    HASH_ITER(hh, hash, elem, tmp) {
+        HASH_DEL(hash, elem); free(elem);
+    }
+    return res;
+}
