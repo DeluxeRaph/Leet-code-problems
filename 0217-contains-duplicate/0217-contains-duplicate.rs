b0024f24167c3021c21d24d5cd500c802ba18e79
Raphael Nembhard <100111587+DeluxeRaph@users.noreply.github.com>
2023-09-15 18:47:10 -0400

    Time: 14 ms (75.03%), Space: 3.8 MB (60.51%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.rs
+++ 0217-contains-duplicate/0217-contains-duplicate.rs
@@ -1,15 +1,16 @@
 use std::collections::HashSet;
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        let mut map = HashSet::new(); // Create the hash set
-
-        for n in nums.iter(){
-            if map.contains(n){
+        // Creates the hash set
+        let mut map = HashSet::new(); 
+        // Iterates through nums first checking if an element in the vector matches an element in the hash set then returns true
+        for &i in nums.iter(){
+            if map.contains(&i){
                 return true;
             }
-
-            map.insert(n);
+        // If the element was not in the Hash set then the element is inserted into the set.
+            map.insert(i);
         }
-        false
+        false // Returns false if duplicates were not found
     }
 }
