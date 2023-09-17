a230fd8f07bb8871a9b00a5cdc5c0bc44334ded5
Raphael Nembhard <100111587+DeluxeRaph@users.noreply.github.com>
2023-09-16 22:53:26 -0400

    Time: 2 ms (78.06%), Space: 2.5 MB (15.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -2,14 +2,17 @@ use std::collections::HashMap;
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::new();
-
-        for (x, y) in nums.into_iter().enumerate() {
-            let diff = target - y;
-
+        // Mapping our current int(n) to an current index(i)
+        for (i, n) in nums.into_iter().enumerate() {
+        // Subtracting our target and (n) 
+            let diff = target - n;
+        // Checking if the diff is in the HashMap and (j) holds the previous index
             if let Some(&j) = map.get(&diff) {
-                return vec![x as i32, j as i32];
+        // Returning index(i) and index(j) that add up to target
+                return vec![i as i32, j as i32];
             } else {
-                map.insert(y, x);
+        // Adding current number and index to Hashmap
+                map.insert(n, i);
             }
         }
         unreachable!()
