b1344b24f64c4fe169eb0564c48b3cedbb31d3fb
Raphael Nembhard <100111587+DeluxeRaph@users.noreply.github.com>
2023-10-22 14:32:29 -0400

    Time: 0 ms (100%), Space: 2.5 MB (99.25%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
@@ -0,0 +1,44 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(root) = root {
+            let mut frontier: Vec<(i32, Rc<RefCell<TreeNode>>)> = vec![(1, root)];
+            let mut max_depth: i32 = 1;
+
+            while let Some((depth, curr)) = frontier.pop() {
+                max_depth = depth.max(max_depth);
+
+                if let Some(left) = curr.borrow_mut().left.take() {
+                    frontier.push((depth + 1, left));
+                }
+
+                if let Some(right) = curr.borrow_mut().right.take() {
+                    frontier.push((depth + 1, right));
+                }
+            }
+
+            max_depth
+        } else {
+            0
+        }
+    }
+}
